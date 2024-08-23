47546c425e6c844234fb39e0f203b323247d1cb9
AbrJA <ajaimes@occ.com.mx>
2024-08-23 17:41:36 -0600

    Using S3

---
+++ R/quantization.R
@@ -0,0 +1,72 @@
+scalar <- function(type = c("int8"), quantile = NULL, always_ram = NULL) {
+  config <- list()
+  checkmate::assert_choice(type, choices = c("int8"))
+  config$type <- type
+  if (!checkmate::test_null(quantile)) {
+    checkmate::assert_number(quantile, lower = 0.5, upper = 1.0)
+    config$quantile <- quantile
+  }
+  if (!checkmate::test_null(always_ram)) {
+    checkmate::assert_flag(always_ram)
+    config$always_ram <- always_ram
+  }
+  structure(list(scalar = config), class = c("config", "scalar"))
+}
+
+product <- function(compression = c("x4", "x8", "x16", "x32", "x64"), always_ram = NULL) {
+  config <- list()
+  checkmate::assert_choice(compression, choices = c("x4", "x8", "x16", "x32", "x64"))
+  config$compression <- compression
+  if (!checkmate::test_null(always_ram)) {
+    checkmate::assert_flag(always_ram)
+    config$always_ram <- always_ram
+  }
+  structure(list(product = config), class = c("config", "product"))
+}
+
+binary <- function(always_ram = NULL) {
+  config <- list()
+  if (!checkmate::test_null(always_ram)) {
+    checkmate::assert_flag(always_ram)
+    config$always_ram <- always_ram
+  }
+  structure(list(binary = config), class = c("config", "binary"))
+}
+
+print.config <- function(object, indent = 1L, first = TRUE) {
+  if (first) {
+    cat("<Config>\n")
+  }
+  str_indent <- strrep("  ", indent)
+  for (name in names(object)) {
+    if (is.list(object[[name]])) {
+      cat(str_indent, name, "\n", sep = "")
+      print.config(object[[name]], indent + 1L, FALSE)
+    } else {
+      cat(str_indent, name, ": ", object[[name]], "\n", sep = "")
+    }
+  }
+}
+
+scalar(type = "int8", quantile = 0.8, TRUE) |>
+  yyjsonr::write_json_str(opts = list(auto_unbox = TRUE, pretty = TRUE)) |>
+  cat()
+
+quantization <- function(binary = NULL, product = NULL, scalar = NULL) {
+  params <- list()
+  if (!checkmate::test_null(binary)) {
+    checkmate::assert_class(binary, classes = c("config", "binary"))
+    params$binary <- binary
+  }
+  if (!checkmate::test_null(product)) {
+    checkmate::assert_class(product, classes = c("config", "product"))
+    params$product <- product
+  }
+  if (!checkmate::test_null(scalar)) {
+    checkmate::assert_class(scalar, classes = c("config", "scalar"))
+    params$scalar <- scalar
+  }
+  structure(list(quantization_config = params), class = c("config", "quantization"))
+}
+
+quantization(binary = binary(TRUE), product = product("x4", FALSE))

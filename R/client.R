cd7a856707e12c2dbca250ce39a2471a5b124fa0
AbrJA <ajaimes@occ.com.mx>
2024-08-28 12:32:33 -0600

    Adding api_key

--- R/client.R
+++ R/client.R
@@ -9,10 +9,14 @@ Client <- R6::R6Class(
   classname = "Client",
   public = list(
     collection = NULL,
-    initialize = function(url) {
+    initialize = function(url, api_key = NULL) {
       checkmate::assertString(url, min.chars = 1L)
       private$.url <- url
       private$.req <- httr2::request(url)
+      if (!checkmate::test_null(api_key)) {
+        private$.req <- private$.req |>
+          httr2::req_headers("api-key" = api_key)
+      }
       self$collection <- Collection$new(private$.req)
     }
   ),
